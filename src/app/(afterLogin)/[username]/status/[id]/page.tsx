9826b3a6ac5aa73616b72eba6097081e4ac6528c
KangYunJi <dbswl701@ajou.ac.kr>
2024-01-09 12:16:30 +0900

    feat: /status/[id] 상세 게시글 페이지

--- src/app/(afterLogin)/[username]/status/[id]/page.tsx
+++ src/app/(afterLogin)/[username]/status/[id]/page.tsx
@@ -1,6 +1,28 @@
-import Image from "next/image";
-import styles from "@/app/(beforeLogin)/_component/main.module.css";
+import BackButton from "@/app/(afterLogin)/_component/BackButton";
+import style from "./singlePost.module.css";
+import Post from "@/app/(afterLogin)/_component/Post";
+import CommentForm from "@/app/(afterLogin)/[username]/status/[id]/_component/CommentForm";
 
 export default function SinglePost() {
-  return <main className={styles.main}>상세 페이지</main>;
+  return (
+    <div className={style.main}>
+      <div className={style.header}>
+        <BackButton />
+        <h3 className={style.headerTitle}>게시하기</h3>
+      </div>
+      <Post />
+      <CommentForm />
+      <div>
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </div>
+    </div>
+  );
 }
