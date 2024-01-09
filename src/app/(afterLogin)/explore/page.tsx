dfc012033075b506ac799d848585b13a2f678b6c
KangYunJi <dbswl701@ajou.ac.kr>
2024-01-09 10:09:56 +0900

    feat: usePathname과 /explore 페이지

--- src/app/(afterLogin)/explore/page.tsx
+++ src/app/(afterLogin)/explore/page.tsx
@@ -1,6 +1,24 @@
-import Image from "next/image";
-import styles from "@/app/(beforeLogin)/_component/main.module.css";
+import SearchForm from "../_component/SearchForm";
+import Trend from "../_component/Trend";
+import style from "./explore.module.css";
 
 export default function Home() {
-  return <main className={styles.main}>검색페이지</main>;
+  return (
+    <main className={style.main}>
+      <div className={style.formZone}>
+        <SearchForm />
+      </div>
+      <div className={style.trend}>
+        <h3>나를 위한 트렌드</h3>
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+      </div>
+    </main>
+  );
 }
