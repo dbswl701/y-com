d0f7795fa81edacea3113df3b5250b9585843257
KangYunJi <dbswl701@ajou.ac.kr>
2024-01-12 14:54:59 +0900

    feat: 로그인 상태에서 /로 가면 home으로 리다이렉트

--- src/app/(beforeLogin)/login/page.tsx
+++ src/app/(beforeLogin)/login/page.tsx
@@ -3,12 +3,18 @@
 import { useRouter } from "next/navigation";
 import Main from "../_component/Main";
 import { useEffect } from "react";
+import { useSession } from "next-auth/react";
 
 export default function Login() {
   const router = useRouter();
-  // router.replace("/i/flow/login");
-  useEffect(() => {
-    router.replace("/i/flow/login");
-  }, []);
+  const { data: session } = useSession();
+
+  if (session) {
+    router.replace("/home");
+    return null;
+  }
+
+  router.replace("/i/flow/login");
+
   return <Main />;
 }
