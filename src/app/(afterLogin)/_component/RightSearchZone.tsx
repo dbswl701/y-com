df4d4937a7f40fd6715c8892410ec9c757cf4482
KangYunJi <dbswl701@ajou.ac.kr>
2024-01-23 00:06:12 +0900

    refactor: URLSearchParams를 이용한 탭 선택 리펙토링

--- src/app/(afterLogin)/_component/RightSearchZone.tsx
+++ src/app/(afterLogin)/_component/RightSearchZone.tsx
@@ -11,18 +11,14 @@ export default function RightSearchZone() {
   const router = useRouter();
 
   const onChangeAll = () => {
-    let url = `/search?q=${searchParams.get(`q`)}`;
-    if (searchParams.has("f")) {
-      url += `&f=${searchParams.get("f")}`;
-    }
-    router.replace(url);
+    const newSearchParams = new URLSearchParams(searchParams);
+    newSearchParams.set("pf", "on");
+    router.replace(`/search?${newSearchParams.toString()}`);
   };
   const onChangeFollow = () => {
-    let url = `/search?q=${searchParams.get(`q`)}&pf=on`;
-    if (searchParams.has("f")) {
-      url += `&f=${searchParams.get("f")}`;
-    }
-    router.replace(url);
+    const newSearchParams = new URLSearchParams(searchParams);
+    newSearchParams.delete("pf");
+    router.replace(`/search?${newSearchParams.toString()}`);
   };
 
   if (pathname === "/explore") return null;
