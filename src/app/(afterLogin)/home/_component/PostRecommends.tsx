82f1ea071bebe4130da7083f55d2b622a65332f8
KangYunJi <dbswl701@ajou.ac.kr>
2024-01-25 16:51:44 +0900

    feat: react-intersection-observer로 더 불러오기

--- src/app/(afterLogin)/home/_component/PostRecommends.tsx
+++ src/app/(afterLogin)/home/_component/PostRecommends.tsx
@@ -4,10 +4,11 @@ import { InfiniteData, useInfiniteQuery } from "@tanstack/react-query";
 import { getPostRecommends } from "../_lib/getPostRecommends";
 import Post from "../../_component/Post";
 import { Post as IPost } from "@/model/Post";
-import { Fragment } from "react";
+import { Fragment, useEffect } from "react";
+import { useInView } from "react-intersection-observer";
 
 export default function PostRecommends() {
-  const { data } = useInfiniteQuery<
+  const { data, fetchNextPage, hasNextPage, isFetching } = useInfiniteQuery<
     IPost[],
     Object,
     InfiniteData<IPost[]>,
@@ -21,11 +22,28 @@ export default function PostRecommends() {
     staleTime: 60000,
     gcTime: 300 * 1000,
   });
-  return data?.pages.map((page, i) => (
-    <Fragment key={i}>
-      {page.map((post) => (
-        <Post key={post.postId} post={post} />
-      ))}{" "}
-    </Fragment>
-  ));
+
+  const { ref, inView } = useInView({
+    threshold: 0,
+    delay: 0,
+  });
+
+  useEffect(() => {
+    if (inView) {
+      !isFetching && hasNextPage && fetchNextPage();
+    }
+  }, [inView, isFetching, fetchNextPage, hasNextPage]);
+
+  return (
+    <>
+      {data?.pages.map((page, i) => (
+        <Fragment key={i}>
+          {page.map((post) => (
+            <Post key={post.postId} post={post} />
+          ))}{" "}
+        </Fragment>
+      ))}
+      <div ref={ref} style={{ height: 50 }} />
+    </>
+  );
 }
