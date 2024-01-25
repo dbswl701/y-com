6a38d4b1ac0ff5b8d75f07353fc8d3e26695dd3f
KangYunJi <dbswl701@ajou.ac.kr>
2024-01-25 18:18:29 +0900

    feat: suspense 안에서 loading 중복 피하기

--- src/app/(afterLogin)/home/_component/PostRecommends.tsx
+++ src/app/(afterLogin)/home/_component/PostRecommends.tsx
@@ -1,6 +1,10 @@
 "use client";
 
-import { InfiniteData, useInfiniteQuery } from "@tanstack/react-query";
+import {
+  InfiniteData,
+  useInfiniteQuery,
+  useSuspenseInfiniteQuery,
+} from "@tanstack/react-query";
 import { getPostRecommends } from "../_lib/getPostRecommends";
 import Post from "../../_component/Post";
 import { Post as IPost } from "@/model/Post";
@@ -9,28 +13,21 @@ import { useInView } from "react-intersection-observer";
 import styles from "@/app/(afterLogin)/home/home.module.css";
 
 export default function PostRecommends() {
-  const {
-    data,
-    fetchNextPage,
-    hasNextPage,
-    isFetching,
-    isPending,
-    isLoading,
-    isError,
-  } = useInfiniteQuery<
-    IPost[],
-    Object,
-    InfiniteData<IPost[]>,
-    [_1: string, _2: string],
-    number
-  >({
-    queryKey: ["posts", "recommends"],
-    queryFn: getPostRecommends,
-    getNextPageParam: (lastPage) => lastPage.at(-1)?.postId,
-    initialPageParam: 0,
-    staleTime: 60000,
-    gcTime: 300 * 1000,
-  });
+  const { data, fetchNextPage, hasNextPage, isFetching } =
+    useSuspenseInfiniteQuery<
+      IPost[],
+      Object,
+      InfiniteData<IPost[]>,
+      [_1: string, _2: string],
+      number
+    >({
+      queryKey: ["posts", "recommends"],
+      queryFn: getPostRecommends,
+      getNextPageParam: (lastPage) => lastPage.at(-1)?.postId,
+      initialPageParam: 0,
+      staleTime: 60000,
+      gcTime: 300 * 1000,
+    });
 
   const { ref, inView } = useInView({
     threshold: 0,
@@ -43,43 +40,6 @@ export default function PostRecommends() {
     }
   }, [inView, isFetching, fetchNextPage, hasNextPage]);
 
-  if (isPending) {
-    return (
-      <div style={{ display: "flex", justifyContent: "center" }}>
-        <svg
-          className={styles.loader}
-          height="100%"
-          viewBox="0 0 32 32"
-          width={40}
-        >
-          <circle
-            cx="16"
-            cy="16"
-            fill="none"
-            r="14"
-            strokeWidth="4"
-            style={{ stroke: "rgb(29, 155, 240)", opacity: 0.2 }}
-          ></circle>
-          <circle
-            cx="16"
-            cy="16"
-            fill="none"
-            r="14"
-            strokeWidth="4"
-            style={{
-              stroke: "rgb(29, 155, 240)",
-              strokeDasharray: 80,
-              strokeDashoffset: 60,
-            }}
-          ></circle>
-        </svg>
-      </div>
-    );
-  }
-
-  if (isError) {
-    return "에러 처리해줘.";
-  }
   return (
     <>
       {data?.pages.map((page, i) => (
